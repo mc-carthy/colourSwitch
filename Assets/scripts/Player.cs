641a55c5bd69412ee9a93c1dc04eccab696b2660
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-05-03 21:56:34 +0100

    Create randomColour function

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -2,12 +2,28 @@
 
 public class Player : MonoBehaviour {
 
+    private readonly string[] colourStrings = { "purple", "orange", "teal", "yellow" };
+    private readonly Color[] colours = {
+        new Color(132, 0, 205, 255),
+        new Color(255, 116, 0, 255),
+        new Color(0, 195, 195, 255),
+        new Color(255, 255, 0, 255)
+    };
+
     private Rigidbody2D rb;
+    private SpriteRenderer sr;
 	private float jumpForce = 10f;
+    private string currentColour;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        SetRandomColour();
     }
 
     private void Update()
@@ -16,6 +32,21 @@ public class Player : MonoBehaviour {
         {
             rb.velocity = Vector2.up * jumpForce;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == currentColour)
+        {
+
+        }
+    }
+
+    private void SetRandomColour()
+    {
+        int index = Random.Range(0, 3);
+        currentColour = colourStrings[index];
+        sr.color = colours[index];
     }
 
 }
---
+++ Assets/scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	private float rotSpeed = 100f;
+
+    private void Update()
+    {
+        transform.Rotate(0, 0, rotSpeed * Time.deltaTime);
+    }
+
+}
