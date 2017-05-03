2da918fccd80b86d63743c4bc061926441a7bd16
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-05-03 19:35:40 +0100

    Implement player jumping

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D rb;
+	private float jumpForce = 10f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+}
